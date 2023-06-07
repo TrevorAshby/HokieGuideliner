aec96d8346c4d238e05e4457034be5abb5ac5243
TrevorAshby <trevor.clark.ashby@gmail.com>
2023-06-07 08:28:40 -0600

    Completed training and saving of larger model

--- guideliner.py
+++ guideliner.py
@@ -1,5 +1,6 @@
 import torch
 import datetime
+import random
 import torch.nn as nn
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
@@ -7,6 +8,14 @@ from transformers import AutoTokenizer, T5ForConditionalGeneration
 
 def train(model, dataloader, tokenizer, num_epochs=10):
     print("Starting model training...")
+
+    input_examples = [
+        "A: Oh, I've read \"1984\". It\'s a classic. Love it. Have you ever read anything by J.K. Rowling? B: Yes, I love the Harry Potter series. Have you read them? A: Yes, I'm a big fan of Harry Potter. I have read all the books and seen all the movies. Do you have a favorite book in the series?|{\"high-level\": {\"topic\": \"literature\", \"if_interest\": \"yes\"}}, {\"middle-level\": {\"topic\": \"book recommendation\", \"if_interest\": \"unknow\"}}, {\"low-level\": {\"topic\": \"J.K. Rowling\", \"if_interest\": \"unknow\"}} {\"high-level\": {\"topic\": \"literature\", \"if_interest\": \"yes\"}}, {\"middle-level\": {\"topic\": \"book recommendation\", \"if_interest\": \"yes\"}, \"low-level\": {\"topic\": \"Harry Potter\", \"if_interest\": \"yes\"}}",
+        "B: What do you like to do? A: I love eating food. I really like new restaurants.|{\"high-level\": {\"topic\": \"food\", \"if_interest\": \"yes\"}, \"low-level\": {\"topic\": \"new restaurant\", \"if_interest\": \"yes\"}}",
+        "A: Have you read any good books lately? B: Yes, I just finished \"The Nightingale\" by Kristin Hannah. It was amazing. A: That sounds interesting. What is it about?|{\"high-level\": {\"topic\": \"books\", \"if_interest\": \"unknow\"}, \"middle-level\": {\"topic\": \"reading\", \"if_interest\": \"unknow\"}} {\"high-level\": {\"topic\": \"books\", \"if_interest\": \"yes\"}, \"middle-level\": {\"topic\": \"novel plot\", \"if_interest\": \"yes\"}}",
+        "B: Oh, I love pasta. What\'s your favorite kind of pasta? A: I'm a big fan of spaghetti with meatballs.|{\"high-level\": {\"topic\": \"food\", \"if_interest\": \"yes\"}, \"middle-level\": {\"topic\": \"cuisine\", \"if_interest\": \"yes\"}, \"low-level\": {\"topic\": \"spaghetti\", \"if_interest\": \"yes\"}}",
+        "A: By the way, have you ever gone hiking before? B: Yeah, I enjoy hiking. Why do you ask? A: I was thinking about going on a hike this weekend, would you be interested in joining me?|{\"high-level\": {\"topic\": \"outdoor activity\", \"if_interest\": \"yes\"}, \"middle-level\": {\"topic\": \"hiking\", \"if_interest\": \"yes\"}}",
+    ]
     criteria = torch.optim.Adam(model.parameters(), lr= 0.000001)
     log = open('./log.txt', 'a+')
     log.write('================ STARTING A NEW RUN == {} =================\n'.format(datetime.datetime.now()))
@@ -16,6 +25,7 @@ def train(model, dataloader, tokenizer, num_epochs=10):
 
         for batch in dataloader:
             x, y = batch
+            # Forward pass
             output = model(input_ids=x.squeeze(1).cuda(), labels=y.squeeze(1).cuda())
             out = output.loss
             out.backward()
@@ -23,13 +33,23 @@ def train(model, dataloader, tokenizer, num_epochs=10):
             eploss += out.item()
 
         if epoch % 1 == 0:
-            in_str = "B:What do you like to do? A:I love eating food. I really like new restaurants.|{\"high-level\": {\"topic\": \"food\", \"if_interest\": \"yes\"}, \"low-level\": {\"topic\": \"new restaurant\", \"if_interest\": \"yes\"}}"
+            log.write("Epoch:{}, EpLoss:{}\n".format(epoch, eploss/len(dataloader)))
+
+            # Generate examples with each input in 'input_examples'
+            for in_str in input_examples:
+                in_ids = tokenizer(in_str, return_tensors='pt').input_ids
+                example = model.generate(in_ids.cuda(), max_new_tokens=50)
+                dec_out = tokenizer.decode(example[0], skip_special_tokens=True)
+                log.write("\tInput:{}, Output:{}\n".format(in_str, dec_out))
+            #log.write("Epoch:{}, EpLoss:{}, Input:\"{}\", Output:\"{}\"\n".format(epoch, eploss/len(dataloader), in_str, dec_out))
+            #print("Epoch:{}, EpLoss:{}, Input:\"{}\", Output:\"{}\"".format(epoch, eploss/len(dataloader), in_str, dec_out))
+            print("Epoch:{}, EpLoss:{}\n".format(epoch, eploss/len(dataloader)))
+            # Generate another random example to print to console
+            in_str = input_examples[random.randrange(1,len(input_examples))]
             in_ids = tokenizer(in_str, return_tensors='pt').input_ids
             example = model.generate(in_ids.cuda(), max_new_tokens=50)
             dec_out = tokenizer.decode(example[0], skip_special_tokens=True)
-            log.write("Epoch:{}, EpLoss:{}, Input:\"{}\", Output:\"{}\"\n".format(epoch, eploss/len(dataloader), in_str, dec_out))
-            print("Epoch:{}, EpLoss:{}, Input:\"{}\", Output:\"{}\"".format(epoch, eploss/len(dataloader), in_str, dec_out))
-    
+            print("Input:{}, Output:{}".format(in_str, dec_out))
     log.close()
 
 class GuidelineDataset(Dataset):
@@ -76,5 +96,5 @@ if __name__ == '__main__':
 
     train(model, dl, tokenizer, 20)
 
-    #TODO Write additional lines of code to save model locally.
+    torch.save(model.state_dict(), './model/guideliner.pt')
     
